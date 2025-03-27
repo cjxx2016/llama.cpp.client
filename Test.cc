86712aa254ddecaa6619769ab51b81e149fdf37e
cjxx <5765389@163.com>
2025-03-27 10:12:11 +0800

    1 change test

--- Test.cc
+++ Test.cc
@@ -4,7 +4,7 @@ int main() {
 
     LlamaClient client("127.0.0.1", 9001);
 
-    client.AddMessage({LlamaClient::SYSTEM, "you are always have a joke about me" });
+    client.AddMessage({LlamaClient::SYSTEM, "you are always have a joke for me" });
 
     std::cout << client.Request("Hello world!") << std::endl;
 
